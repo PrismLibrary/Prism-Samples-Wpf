c71a0e0e5032e37e7d8a2e8ef4077cab7bdb907c
Brian Lagunas <brianlagunas@users.noreply.github.com>
2018-09-25 14:18:31 -0600

    updated sample 18

--- 18-NavigationCallback/BasicRegionNavigation/App.xaml.cs
+++ 18-NavigationCallback/BasicRegionNavigation/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BasicRegionNavigation.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
 using System.Windows;
 
 namespace BasicRegionNavigation
@@ -11,14 +9,21 @@ namespace BasicRegionNavigation
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override Window CreateShell()
         {
-            base.OnStartup(e);
+            return Container.Resolve<MainWindow>();
+        }
 
-            var bootstrapper = new Bootstrapper();
-            bootstrapper.Run();
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleA.ModuleAModule>();
         }
     }
 }
