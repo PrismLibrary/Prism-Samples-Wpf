be606df932160908632d543e2ce59b3275a494d1
Brian Lagunas <brianlagunas@users.noreply.github.com>
2018-09-24 22:33:14 -0600

    updated sample 14

--- 14-UsingEventAggregator/UsingEventAggregator/App.xaml.cs
+++ 14-UsingEventAggregator/UsingEventAggregator/App.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using UsingEventAggregator.Views;
 
 namespace UsingEventAggregator
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        protected override void OnStartup(StartupEventArgs e)
+        protected override Window CreateShell()
         {
-            base.OnStartup(e);
+            return Container.Resolve<MainWindow>();
+        }
 
-            var bootstrapper = new Bootstrapper();
-            bootstrapper.Run();
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleA.ModuleAModule>();
+            moduleCatalog.AddModule<ModuleB.ModuleBModule>();
         }
     }
 }
