182376c7c765e4b86b9d6b6a973682b8f6b1b5e9
Brian Lagunas <brianlagunas@users.noreply.github.com>
2018-09-24 21:55:44 -0600

    updated sample 9

--- 07-Modules - LoadManual/ModuleA/ModuleAModule.cs
+++ 07-Modules - LoadManual/ModuleA/ModuleAModule.cs
@@ -7,16 +7,10 @@ namespace ModuleA
 {
     public class ModuleAModule : IModule
     {
-        IRegionManager _regionManager;
-
-        public ModuleAModule(RegionManager regionManager)
-        {
-            _regionManager = regionManager;
-        }
-
         public void OnInitialized(IContainerProvider containerProvider)
         {
-            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
