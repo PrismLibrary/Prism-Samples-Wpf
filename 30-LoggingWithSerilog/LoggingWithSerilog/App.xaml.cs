bc55ba9e5eb517b4fd87c45a9c53f4dc3ce22efe
Caio Proiete <caio.proiete@gmail.com>
2019-10-24 18:10:34 -0300

    Add sample project `LoggingWithSerilog` (Prism:ILoggerFacade forwarding logs to Serilog:ILogger)

---
+++ 30-LoggingWithSerilog/LoggingWithSerilog/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using Prism.Ioc;
+using Serilog;
+using LoggingWithSerilog.Views;
+
+namespace LoggingWithSerilog
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(path: "DemoLog.txt", encoding: Encoding.UTF8)
+                .CreateLogger();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.CloseAndFlush();
+
+            base.OnExit(e);
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSerilog();
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
