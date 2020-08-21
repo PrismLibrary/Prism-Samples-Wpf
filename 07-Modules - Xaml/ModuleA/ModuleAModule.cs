5769453b834ae8e2aabc9fa6bccf5c08124d687a
Brian Lagunas <brianlagunas@users.noreply.github.com>
2020-08-21 16:13:14 -0600

    adding XAML catalog example

---
+++ 07-Modules - Xaml/ModuleA/ModuleAModule.cs
@@ -0,0 +1,21 @@
+﻿using ModuleA.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ 07-Modules - Xaml/Modules/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Modules.Views;
+using Prism.Modularity;
+using Prism.Unity;
+using Prism.Ioc;
+using System.Windows;
+using System;
+
+namespace Modules
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return ModuleCatalog.CreateFromXaml(new Uri("/Modules;component/ModuleCatalog.xaml", UriKind.Relative));
+        }
+    }
+}
