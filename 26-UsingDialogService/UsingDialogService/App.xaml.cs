b2c9befc97190a4c9645dafd76c677ad2145e086
niimi <niimi@dcinc.co.jp>
2022-09-04 00:08:00 +0900

    Create Default Prism Solution for UsingDialogService

---
+++ 26-UsingDialogService/UsingDialogService/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using UsingDialogService.Views;
+
+namespace UsingDialogService
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
