25515bd73c97610ba2cb70ce285dda599a2722a0
niimima <niimi@dcinc.co.jp>
2023-02-27 18:48:19 +0900

    Add StylingDialog.sln

---
+++ 27-StylingDialog/StylingDialog/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using StylingDialog.Views;
+using System.Windows;
+
+namespace StylingDialog
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
